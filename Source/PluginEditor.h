49ed0ef313e54ae0cfe28b59b3c69d5ff338f3ff
Micah Scott <micah@scanlime.org>
2022-04-16 22:58:33 -0700

    trim out template code

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,33 +1,23 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #pragma once
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-//==============================================================================
-/**
-*/
-class RevertebratorAudioProcessorEditor  : public juce::AudioProcessorEditor
+class RevertebratorAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    RevertebratorAudioProcessorEditor (RevertebratorAudioProcessor&);
+    RevertebratorAudioProcessorEditor(RevertebratorAudioProcessor&);
     ~RevertebratorAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
     RevertebratorAudioProcessor& audioProcessor;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RevertebratorAudioProcessorEditor)
+    juce::TextButton dataFileButton{"Open..."};
+    juce::FileChooser dataFileChooser{"Choose a data file...",
+	    juce::File::getSpecialLocation(juce::File::SpecialLocationType::currentApplicationFile), "*.json"};
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(RevertebratorAudioProcessorEditor)
 };
