1e8e3f9f5ffb75ff841530c72bdd1e7c226620cf
Micah Scott <micah@scanlime.org>
2022-04-21 20:15:37 -0700

    plausibly threadsafe storage for grain db

--- Source/GrainData.cpp
+++ Source/GrainData.cpp
@@ -12,77 +12,129 @@ GrainData::GrainData() {
 
 GrainData::~GrainData() {}
 
+GrainData::Accessor::Accessor(GrainData &data)
+    : ref(data), reader(data.rwLock) {}
+
+bool GrainData::Accessor::read(float *const *destChannels, int numDestChannels,
+                               juce::int64 startSampleInSource,
+                               int numSamplesToRead) {
+  return ref.state->reader->read(destChannels, numDestChannels,
+                                 startSampleInSource, numSamplesToRead);
+}
+
+int GrainData::Accessor::sampleRate() { return ref.state->sampleRate; }
+
+float GrainData::Accessor::maxGrainWidth() { return ref.state->maxGrainWidth; }
+
+int GrainData::Accessor::numBins() {
+  return juce::jmin(ref.state->binF0.size(), ref.state->binX.size() - 1);
+}
+
+int GrainData::Accessor::numGrains() { return ref.state->grainX.size(); }
+
+juce::Range<int> GrainData::Accessor::grainsForBin(int bin) {
+  bin = juce::jlimit(0, numBins() - 1, bin);
+  return juce::Range<int>(ref.state->binX[bin], ref.state->binX[bin + 1]);
+}
+
+juce::int64 GrainData::Accessor::centerSampleForGrain(int grain) {
+  return ref.state->grainX[juce::jlimit(0, numGrains() - 1, grain)];
+}
+
+float GrainData::Accessor::pitchForBin(int bin) {
+  return ref.state->binF0[juce::jlimit(0, numBins() - 1, bin)];
+}
+
+int GrainData::Accessor::closestBinForPitch(float hz) {
+  auto begin = ref.state->binF0.begin(), end = ref.state->binF0.end();
+  int bin1 = std::lower_bound(begin, end, hz) - begin;
+  int bin0 = bin1 - 1;
+  float dist0 = fabs(pitchForBin(bin0) - hz);
+  float dist1 = fabs(pitchForBin(bin1) - hz);
+  return (dist0 < dist1) ? bin0 : bin1;
+}
+
 void GrainData::valueChanged(juce::Value &) {
+  juce::ScopedReadLock reader(rwLock);
   auto newSrc = src.toString();
-  if (newSrc != srcFile.getFullPathName()) {
-    srcFile = newSrc;
-    reload();
+  if (!state || newSrc != state->srcFile.getFullPathName()) {
+    load(newSrc);
   }
 }
 
-void GrainData::reload() {
-  if (!srcFile.existsAsFile()) {
+void GrainData::load(juce::String &srcFile) {
+  auto newState = std::make_unique<State>();
+  newState->srcFile = srcFile;
+
+  if (!newState->srcFile.existsAsFile()) {
     status.setValue("No grain data file");
     return;
   }
 
-  auto json = JSON::parse(srcFile);
+  auto json = JSON::parse(newState->srcFile);
   if (!json.isObject()) {
     status.setValue("Failed to load JSON data!");
     return;
   }
 
-  soundFile =
-      srcFile.getSiblingFile(json.getProperty("sound_file", var()).toString());
-  if (!soundFile.existsAsFile()) {
-    status.setValue("Can't find sound file: " + soundFile.getFullPathName());
+  newState->soundFile = newState->srcFile.getSiblingFile(
+      json.getProperty("sound_file", var()).toString());
+  if (!newState->soundFile.existsAsFile()) {
+    status.setValue("Can't find sound file: " +
+                    newState->soundFile.getFullPathName());
     return;
   }
 
   juce::AudioFormatManager formats;
   formats.registerBasicFormats();
-  reader = std::unique_ptr<juce::AudioFormatReader>(
-      formats.createReaderFor(soundFile));
-  if (!reader) {
+  newState->reader = std::unique_ptr<juce::AudioFormatReader>(
+      formats.createReaderFor(newState->soundFile));
+  if (!newState->reader) {
     status.setValue("Can't read from sound file");
     return;
   }
 
-  soundLen = json.getProperty("sound_len", var());
-  maxGrainWidth = json.getProperty("max_grain_width", var());
-  sampleRate = json.getProperty("sample_rate", var());
+  newState->soundLen = json.getProperty("sound_len", var());
+  newState->maxGrainWidth = json.getProperty("max_grain_width", var());
+  newState->sampleRate = json.getProperty("sample_rate", var());
   auto varBinX = json.getProperty("bin_x", var());
   auto varBinF0 = json.getProperty("bin_f0", var());
   auto varGrainX = json.getProperty("grain_x", var());
 
   // Bin index (int list)
-  binX.clear();
   if (varBinX.isArray()) {
     for (auto x : *varBinX.getArray()) {
-      binX.add(x);
+      newState->binX.add(x);
     }
   }
 
   // Bin fundamental frequencies (float list)
-  binF0.clear();
   if (varBinF0.isArray()) {
     for (auto f0 : *varBinF0.getArray()) {
-      binF0.add(f0);
+      newState->binF0.add(f0);
     }
   }
 
   // Grain index (int64le array in a base64 string)
-  grainX.clear();
   juce::MemoryOutputStream memGrainX;
   if (juce::Base64::convertFromBase64(memGrainX, varGrainX.toString())) {
     juce::MemoryInputStream in(memGrainX.getMemoryBlock());
     while (!in.isExhausted()) {
-      grainX.add(in.readInt64());
+      newState->grainX.add(in.readInt64());
     }
   }
 
-  status.setValue(String(sampleRate) + " Hz, " + String(soundLen / 1e6) +
-                  " Msamples, width " + String(maxGrainWidth) + " sec, " +
-                  String(binX.size()) + " bins, " + String(grainX.size()) +
-                  " grains");
+  auto newStatus = newState->toString();
+
+  {
+    juce::ScopedReadLock writer(rwLock);
+    std::swap(newState, state);
+  }
+  status.setValue(newStatus);
+}
+
+String GrainData::State::toString() const {
+  return String(sampleRate) + " Hz, " + String(soundLen / 1e6) +
+         " Msamples, width " + String(maxGrainWidth) + " sec, " +
+         String(binX.size()) + " bins, " + String(grainX.size()) + " grains";
 }
--- Source/GrainData.h
+++ Source/GrainData.h
@@ -9,18 +9,47 @@ public:
 
   juce::Value src, status;
 
+  class Accessor {
+  public:
+    Accessor(GrainData &data);
+
+    bool read(float *const *destChannels, int numDestChannels,
+              juce::int64 startSampleInSource, int numSamplesToRead);
+
+    int sampleRate();
+    float maxGrainWidth();
+    int numBins();
+    int numGrains();
+
+    juce::int64 centerSampleForGrain(int grain);
+    float pitchForBin(int bin);
+    int closestBinForPitch(float hz);
+    juce::Range<int> grainsForBin(int bin);
+
+  private:
+    GrainData &ref;
+    juce::ScopedReadLock reader;
+  };
+
 private:
   void valueChanged(juce::Value &) override;
-  void reload();
+  void load(juce::String &);
 
-  juce::File srcFile, soundFile;
-  std::unique_ptr<juce::AudioFormatReader> reader;
-  juce::int64 soundLen;
-  float maxGrainWidth;
-  int sampleRate;
-  juce::Array<int> binX;
-  juce::Array<float> binF0;
-  juce::Array<juce::int64> grainX;
+  struct State {
+    juce::File srcFile, soundFile;
+    std::unique_ptr<juce::AudioFormatReader> reader;
+    juce::int64 soundLen;
+    float maxGrainWidth;
+    int sampleRate;
+    juce::Array<int> binX;
+    juce::Array<float> binF0;
+    juce::Array<juce::int64> grainX;
+
+    juce::String toString() const;
+  };
+
+  juce::ReadWriteLock rwLock;
+  std::unique_ptr<State> state;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GrainData)
 };
