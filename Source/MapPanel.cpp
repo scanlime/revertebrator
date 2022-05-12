455ab71e56afa51ad7ec1e71d6930c71ac5d8292
Micah Scott <micah@scanlime.org>
2022-05-12 12:16:46 -0700

    fix crash on click without index

--- Source/MapPanel.cpp
+++ Source/MapPanel.cpp
@@ -389,9 +389,13 @@ void MapPanel::requestNewImage() {
 void MapPanel::updateGrainUnderMouse(const juce::MouseEvent &e, bool on) {
   auto source = e.source.getIndex();
   auto index = processor.grainData.getIndex();
-  Layout layout(getLocalBounds().toFloat(), *index);
-  auto point = layout.pointInfo(e.getEventRelativeTo(this).position);
-  processor.mouseInputForGrain(point.grain, on && point.valid, source);
+  if (index == nullptr) {
+    processor.mouseInputForGrain(0, false, source);
+  } else {
+    Layout layout(getLocalBounds().toFloat(), *index);
+    auto point = layout.pointInfo(e.getEventRelativeTo(this).position);
+    processor.mouseInputForGrain(point.grain, on && point.valid, source);
+  }
 }
 
 void MapPanel::changeListenerCallback(juce::ChangeBroadcaster *) {
