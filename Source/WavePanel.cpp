ec555c050f8a166156fbdfbf8b318ce168e0f4f7
Micah Scott <micah@scanlime.org>
2022-05-12 20:52:17 -0700

    working on wave display

--- Source/WavePanel.cpp
+++ Source/WavePanel.cpp
@@ -1,4 +1,52 @@
 #include "WavePanel.h"
+#include <unordered_map>
+
+class WaveCollector {
+public:
+  void updateVoice(const GrainVoice &voice, GrainWaveform &wave, float gain,
+                   int sampleNum) {
+    std::lock_guard<std::mutex> guard(voicesMutex);
+    auto &state = voices[&voice];
+    state.wave = wave;
+    state.gain = gain;
+    state.sampleNum = sampleNum;
+  }
+
+  struct WaveState {
+    GrainWaveform::Ptr wave;
+    float totalGain{0.f};
+    std::vector<int> positionForEachVoice;
+  };
+
+  using Waves = std::unordered_map<const GrainWaveform *, WaveState>;
+
+  Waves collect() {
+    Voices collectedVoices;
+    Waves collectedWaves;
+    {
+      std::lock_guard<std::mutex> guard(voicesMutex);
+      collectedVoices.swap(voices);
+    }
+    for (auto &voice : collectedVoices) {
+      auto &state = collectedWaves[voice.second.wave.get()];
+      state.wave = voice.second.wave;
+      state.totalGain += voice.second.gain;
+      state.positionForEachVoice.push_back(voice.second.sampleNum);
+    }
+    return collectedWaves;
+  }
+
+private:
+  struct VoiceState {
+    GrainWaveform::Ptr wave;
+    float gain;
+    int sampleNum;
+  };
+
+  using Voices = std::unordered_map<const GrainVoice *, VoiceState>;
+  std::mutex voicesMutex;
+  Voices voices;
+};
 
 class WavePanel::ImageRender : public juce::Thread,
                                public juce::ChangeBroadcaster,
@@ -23,14 +71,6 @@ public:
     }
   }
 
-  void grainVoicePlaying(const GrainVoice &voice, const GrainSound &sound,
-                         GrainWaveform &wave, const GrainSequence::Point &seq,
-                         int sampleNum) override {
-    // std::lock_guard<std::mutex> guard(wavesMutex);
-    // auto &waveInfo = waves.getReference(WaveInfo::Key(&wave));
-    // waveInfo.ptr = wave;
-  }
-
   void run() override {
     static constexpr int fps = 20;
     while (!threadShouldExit()) {
@@ -38,29 +78,48 @@ public:
       auto nextImage = renderImage();
       {
         std::lock_guard<std::mutex> guard(imageMutex);
-        image = std::move(nextImage);
+        std::swap(image, nextImage);
       }
       sendChangeMessage();
     }
   }
 
+  void grainVoicePlaying(const GrainVoice &voice, const GrainSound &sound,
+                         GrainWaveform &wave, const GrainSequence::Point &seq,
+                         int sampleNum) override {
+    waves.updateVoice(voice, wave, seq.gain, sampleNum);
+  }
+
 private:
   std::mutex requestMutex;
   Request request;
-
   std::mutex imageMutex;
   std::unique_ptr<juce::Image> image;
+  WaveCollector waves;
 
   Request latestRequest() {
     std::lock_guard<std::mutex> guard(requestMutex);
     return request;
   }
 
+  struct ColumnInfo {};
+
   std::unique_ptr<juce::Image> renderImage() {
     auto req = latestRequest();
     auto width = req.bounds.getWidth(), height = req.bounds.getHeight();
     auto image = std::make_unique<juce::Image>(
         juce::Image::RGB, std::max(1, width), std::max(1, height), false);
+
+    auto collectedWaves = waves.collect();
+    std::vector<ColumnInfo> columns(width);
+    for (auto &wave : collectedWaves) {
+      printf("Wave %p, %d positions, gain %f, window %f %d %d %d\n",
+             wave.second.wave.get(), wave.second.positionForEachVoice.size(),
+             wave.second.totalGain, wave.second.wave->key.window.mix,
+             wave.second.wave->key.window.width0,
+             wave.second.wave->key.window.width1,
+             wave.second.wave->key.window.phase1);
+    }
 
     juce::Image::BitmapData bits(*image, juce::Image::BitmapData::writeOnly);
     for (int y = 0; y < height; y++) {
