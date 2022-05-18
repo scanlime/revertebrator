e523aad267f85e1be613be33315ad7a313890471
Micah Scott <micah@scanlime.org>
2022-05-18 16:45:32 -0700

    another! mess!

--- Source/WavePanel.cpp
+++ Source/WavePanel.cpp
@@ -53,23 +53,35 @@ public:
     auto image = std::make_unique<juce::Image>(juce::Image::RGB, width(),
                                                height(), false);
 
-    juce::Graphics g(*image);
-    g.fillAll(params.background);
-    g.setColour(params.background.contrasting(1.f));
+    // Each sorted active grain gets a row in the backing image
+    auto waves = collectWavesSortedByGrain();
+    if (waves.size() > 0) {
+      for (int i = 0; i < waves.size(); i++) {
+        drawWaveRow(*image, *waves[i], i * height() / waves.size(),
+                    (i + 1) * height() / waves.size());
+      }
+    } else {
+      juce::Graphics g(*image);
+      g.setColour(params.background);
+      g.fillAll();
+    }
 
     // Faint outline of all active window functions
+    juce::Graphics g(*image);
+    g.setColour(params.background.contrasting(1.f));
+    g.setOpacity(0.2f);
     for (auto window : collectUniqueWaveformWindows()) {
-      g.setOpacity(0.2f);
       g.strokePath(pathForWindow(window), juce::PathStrokeType(2.f));
     }
 
     // Highlighted window functions
     g.setColour(params.highlight);
+    g.setOpacity(0.4f);
     for (auto &window : state.windows) {
-      g.setOpacity(0.4f);
       g.strokePath(pathForWindow(window), juce::PathStrokeType(3.5f));
     }
 
+    // Center column marker is topmost
     g.setColour(params.highlight);
     drawCenterColumn(g);
 
@@ -81,6 +93,39 @@ private:
   int height() const { return params.bounds.getHeight(); }
   int centerColumn() const { return width() / 2; }
   float samplesPerColumn() const { return 2 * state.widthInSamples / width(); }
+
+  void drawWaveRow(juce::Image &image, const WaveInfo &waveInfo, int top,
+                   int bottom) {
+    const auto &wave = *waveInfo.wave;
+
+// xx automatic gain control where
+
+    // Make a big gain coverage map for this waveform, with sample resolution
+    std::vector<float> coverage(wave.getNumSamples());
+    for (const auto &playback : wave.playing) {
+      const auto &gains = playback.seq.gains;
+      auto totalGain = std::accumulate(gains.begin(), gains.end(), 0.f);
+      auto begin = std::max(0, playback.samples.getBegin());
+      auto end = std::min(coverage.size()-1, playback.samples.getEnd());
+      for (auto x = begin; x < end; x++) {
+        coverage[x] += totalGain;
+      }
+    }
+
+    // Downsample to the image's width
+
+}
+
+    std;
+
+    juce::Image::BitmapData bits(image, juce::Image::BitmapData::writeOnly);
+    for (int x = 0; x < width(); x++) {
+      for (int y = top; y < bottom; y++) {
+        auto c = params.background.contrasting(0.5f);
+        bits.setPixelColour(x, y, c);
+      }
+    }
+  }
 
   void drawCenterColumn(juce::Graphics &g) {
     auto thick = 2.5f, margin = 1.f;
@@ -100,6 +145,18 @@ private:
         result.push_back(w);
       }
     }
+    return result;
+  }
+
+  std::vector<const WaveInfo *> collectWavesSortedByGrain() const {
+    std::vector<const WaveInfo *> result;
+    for (auto &item : state.waves) {
+      result.push_back(&item.second);
+    }
+    std::sort(result.begin(), result.end(),
+              [](const WaveInfo *a, const WaveInfo *b) {
+                return a->wave->key.grain > b->wave->key.grain;
+              });
     return result;
   }
 
@@ -160,11 +217,18 @@ public:
 
   void run() override {
     static constexpr int approxFrameRateLimit = 30;
+    static constexpr float imagePersistence = 0.6f;
+
     while (!threadShouldExit()) {
       auto nextImage = renderImage();
       if (nextImage != nullptr) {
         {
           std::lock_guard<std::mutex> guard(imageMutex);
+          if (image != nullptr) {
+            juce::Graphics g(*nextImage);
+            g.setOpacity(imagePersistence);
+            g.drawImage(*image, nextImage->getBounds().toFloat());
+          }
           std::swap(image, nextImage);
         }
         sendChangeMessage();
